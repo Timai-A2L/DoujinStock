e6be9e80e6fa4ebe6a405850c766656928bf2605
Timai <t.0620.imai@gmail.com>
2018-05-18 09:06:49 +0900

    Fix move to Fullscreen

--- DoujinStock/ContainerView.swift
+++ DoujinStock/ContainerView.swift
@@ -9,34 +9,55 @@ import UIKit
 
 class ContainerView: UIView, UIGestureRecognizerDelegate {
   
-  var saveOriginY: CGFloat = 0.0
+  var saveOriginX: CGFloat = 0.0
   var parentView: SwitcherView
   var isFullScreen: Bool = false
   var index: Int = 0
+  var savedTranform: CATransform3D = CATransform3DIdentity
   var tapRecognizer: UITapGestureRecognizer!
+  
+  convenience init(frame: CGRect, parentView: SwitcherView, imageName: String) {
+    self.init(frame: frame, parentView: parentView)
+    self.layer.contents = UIImage(named: imageName)!.cgImage!
+  }
   
   init(frame: CGRect, parentView: SwitcherView) {
     self.parentView = parentView
     super.init(frame: frame)
     self.parentView = parentView
-    saveOriginY = frame.origin.y
+    saveOriginX = frame.origin.x
     
     tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleAnimation))
     tapRecognizer.numberOfTapsRequired = 1
     
     addGestureRecognizer(tapRecognizer)
     tapRecognizer.delegate = self
+    
+    layer.shadowColor = UIColor.black.cgColor
+    layer.shadowOpacity = 0.5
+    layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+    layer.shadowRadius = 20.0
+    
+    layer.edgeAntialiasingMask = CAEdgeAntialiasingMask(rawValue:
+    CAEdgeAntialiasingMask.layerLeftEdge.rawValue |
+    CAEdgeAntialiasingMask.layerRightEdge.rawValue |
+    CAEdgeAntialiasingMask.layerBottomEdge.rawValue |
+    CAEdgeAntialiasingMask.layerTopEdge.rawValue)
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  func toggleAnimation() {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith shouldRecognizeSimulataneouslyWithGestureRecognizer: UIGestureRecognizer) -> Bool {
+    return true
+  }
+  
+  @objc func toggleAnimation() {
     isFullScreen = !isFullScreen
     parentView.animate(view: self, isFullScreen: isFullScreen)
     parentView.scrollView.isScrollEnabled = !isFullScreen
-    if !SwitcherView.enableUserIntaractionInSwitcher {
+    if !SwitcherView.enableUserInteractionInSwitcher {
       for subView in subviews {
         subView.isUserInteractionEnabled = isFullScreen
       }
--- DoujinStock/SwitcherView.swift
+++ DoujinStock/SwitcherView.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class SwitcherView: UIView, UIScrollViewDelegate {
   
+  static let enableUserInteractionInSwitcher = true
+  
   var scrollView: UIScrollView!
   var imageView: UIImageView!
+  var containerViews: [ContainerView] = []
   
   let angle: CGFloat = 55.0
   let scaleOut: CGFloat = 0.8
   
   var switcherViewPadding: CGFloat = 0.0
   let separatorDivisor: CGFloat = 3.0
+  var saveContentSize: CGSize!
+  
+  var translationDivisor: CGFloat {
+    get {
+      if UIDevice.current.userInterfaceIdiom == .pad {
+        return 4.46
+      } else {
+        return 4.13
+      }
+    }
+  }
+  
   let imageName: String = "image1.jpeg"
   var image: UIImage!
   
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     standardInitialize()
@@ -33,10 +49,10 @@ class SwitcherView: UIView, UIScrollViewDelegate {
   func standardInitialize() {
     layer.backgroundColor = UIColor.darkGray.cgColor
     
-    image = UIImage(named: imageName)
-    imageView = UIImageView(image: image)
-    imageView.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
-    imageView.layer.transform = getTransform(translatedToX: 0, isScale:true, isRotate:true)
+    //image = UIImage(named: imageName)
+    //imageView = UIImageView(image: image)
+    //imageView.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
+    //imageView.layer.transform = getTransform(translatedToX: 0, isScale:true, isRotate:true)
     
     switcherViewPadding = bounds.size.height / separatorDivisor
     scrollView = UIScrollView(frame: bounds)
@@ -44,16 +60,34 @@ class SwitcherView: UIView, UIScrollViewDelegate {
     scrollView.minimumZoomScale = 1.0
     scrollView.maximumZoomScale = 1.0
     scrollView.isScrollEnabled = true
-    scrollView.contentSize = CGSize(width: bounds.size.width+imageView.frame.size.width, height: bounds.size.height)
+    scrollView.contentSize = CGSize(width: bounds.size.width, height: bounds.size.height)
     scrollView.delegate = self
     
     
     addSubview(scrollView)
-    scrollView.addSubview(imageView)
+    //scrollView.addSubview(imageView)
+    
+    addContainerView(ContainerView(frame: bounds,
+                                   parentView: self,
+                                   imageName: "image1.jpeg"))
   }
   
   func radians(degree: CGFloat) -> CGFloat {
     return degree * CGFloat.pi / 180.0
+  }
+  
+  func addContainerView(_ view: ContainerView) {
+    view.layer.shadowColor = UIColor.black.cgColor
+    view.frame = CGRect(x: CGFloat(scrollView.subviews.count) * switcherViewPadding,
+                        y: 0.0,
+                        width: frame.size.width,
+                        height: frame.size.height)
+    scrollView.addSubview(view)
+    view.index = containerViews.count
+    containerViews.append(view)
+    view.layer.transform = getTransform(translatedToX: 0, isScale: true, isRotate: true)
+    scrollView.contentSize = CGSize(width: scrollView.contentSize.width + switcherViewPadding,
+                                    height:scrollView.contentSize.height)
   }
   
   func getTransform(translatedToX: CGFloat, isScale: Bool, isRotate: Bool) -> CATransform3D {
@@ -66,9 +100,46 @@ class SwitcherView: UIView, UIScrollViewDelegate {
       transform = CATransform3DScale(transform, scaleOut, scaleOut, scaleOut)
     }
     if translatedToX != 0 {
-      transform = CATransform3DTranslate(transform, 0.0, 0.0, translatedToX)
+      transform = CATransform3DTranslate(transform, translatedToX, 0.0, 0.0)
     }
     return transform
   }
   
+  func animate(transform: CATransform3D, view: UIView) {
+    let basicAnim = CABasicAnimation(keyPath: "transform")
+    basicAnim.fromValue = view.layer.transform
+    basicAnim.toValue = transform
+    view.layer.add(basicAnim, forKey: nil)
+    CATransaction.setDisableActions(true)
+    view.layer.transform = transform
+    CATransaction.setDisableActions(false)
+  }
+  
+  func animate(view: ContainerView, isFullScreen: Bool) {
+    if isFullScreen {
+      for i in 0..<view.index {
+        animate(transform: getTransform(translatedToX: -bounds.size.width, isScale: true, isRotate: false), view: containerViews[i])
+      }
+      animate(transform: getTransform(translatedToX: scrollView.contentOffset.x,
+                                      isScale: false, isRotate: false), view: view)
+      //saveContentSize = scrollView.contentSize
+      //scrollView.contentSize = CGSize(width: bounds.size.width, height: bounds.size.height)
+      for i in (view.index+1)..<containerViews.count {
+        animate(transform: getTransform(translatedToX: 2.0 * bounds.size.width,
+                                        isScale: true, isRotate: true), view: containerViews[i])
+      }
+    } else {
+      for i in 0..<view.index {
+        animate(transform: getTransform(translatedToX: 0.0,
+                                        isScale: true, isRotate: true), view: containerViews[i])
+      }
+      animate(transform: getTransform(translatedToX: 0.0,
+                                      isScale: true, isRotate: true), view: view)
+      //scrollView.contentSize = saveContentSize
+      for i in (view.index + 1)..<containerViews.count {
+        animate(transform: getTransform(translatedToX: 0.0,
+                                        isScale: true, isRotate: true), view: containerViews[i])
+      }
+    }
+  }
 }
